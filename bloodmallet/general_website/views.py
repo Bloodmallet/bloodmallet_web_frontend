338b25c0b47b16d61d5fee5bc0775f879a26bd7a
Bloodmallet <kruse.peter.1990@gmail.com>
2018-11-15 12:55:59 +0100

    add accessible error page during debug

--- bloodmallet/general_website/views.py
+++ bloodmallet/general_website/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from .forms import NameForm
 
 
+def error(request, code: int=404, message: str="Page not found"):
+    error = {
+        'error': {
+            'code': code,
+            'text': message
+        }
+    }
+    return render(request, 'general_website/error.html', error, status=code)
+
+# define custom error pages
+def handler404(request, exception, template_name='general_website/error.html'):
+    return error(request, 404, "Page not found")
+
+def handler500(request, exception, template_name='general_website/error.html'):
+    return error(request, 500, "Something went wrong. Try again later")
+
+
+
 def index(request):
-    return render(request, "general_website/default.html", {'text': "Sir!", 'form': NameForm()})
+    return render(request, 'general_website/default.html', {'text': "Sir!", 'form': NameForm()})
