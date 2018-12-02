d4be3b6bb8061b141eb00892134aa5650b6a920a
Bloodmallet <kruse.peter.1990@gmail.com>
2018-12-02 10:52:13 +0100

    add receiver examples, which will need a lot more refinement
    Goal: track and add patreon level automatically on login and connect.
    This will require a profile model to hold this data.

--- bloodmallet/general_website/models.py
+++ bloodmallet/general_website/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in
+from allauth.socialaccount.signals import pre_social_login, social_account_updated
+
 
 # Create your models here.
+
+@receiver([pre_social_login, social_account_updated])
+def update_pledge_level(sender, sociallogin, **kwargs):
+    """Checks kwargs for pledge level in account data.
+
+    Arguments:
+        sender {[type]} -- [description]
+        sociallogin {[type]} -- [description]
+    """
+
+    print(sender)
+    print(sociallogin)
+    print(sociallogin.account) # read social allauth models.py
+
+#https://stackoverflow.com/questions/40684838/django-django-allauth-save-extra-data-from-social-login-in-signal
+
+@receiver(user_logged_in)
+def update_user_information(sender, request, user, **kwargs):
+    """Login triggers a check for the patreon level.
+
+    Arguments:
+        sender {django.contrib.auth.models.User} -- [description]
+        request {} -- [description]
+        user {User} -- User model data blop
+    """
+
+    print("User logged in!")
+    print(sender)
+    print(request)
+    print(user)
+    print(user.email)
+# https://docs.patreon.com/#fetching-a-patron-39-s-profile-info
