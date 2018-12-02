fb1c0b1fc8537774d9f7a73fd2e383f6caed4df8
Bloodmallet <kruse.peter.1990@gmail.com>
2018-12-02 20:19:43 +0100

    add general setttings and connections url

--- bloodmallet/general_website/urls.py
+++ bloodmallet/general_website/urls.py
@@ -2,14 +2,18 @@ from django.urls import path, include
 
 from . import views
 
+from allauth.socialaccount.views import ConnectionsView
+
 urlpatterns = [
     path('', views.index, name='index_empty'),
     path('index.html', views.index, name='index_long'),
     path('index', views.index, name='index'),
     path('error', views.error, name='error'),
+    path('settings/general', views.settings, name='settings'),
 
     # allauth account actions
     path('accounts/', include('allauth.urls')),
+    path('settings/connections', ConnectionsView.as_view(), name='account_connections'),
 
     # account actions
     path('login', views.login, name='login'),
