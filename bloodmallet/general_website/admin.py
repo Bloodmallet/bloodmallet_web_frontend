4fb64da82ec79f63cd332c06ae40521ad00779e6
Bloodmallet <kruse.peter.1990@gmail.com>
2019-02-28 22:28:53 +0100

    add models to admin

--- bloodmallet/general_website/admin.py
+++ bloodmallet/general_website/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from general_website.models import Faction, Teleporter, Profile, Race, WowClass, WowSpec, FightStyle, SimulationType, Simulation, Queue, Result, GeneralResult
+
 # Register your models here.
+admin.site.register(Faction)
+admin.site.register(Teleporter)
+admin.site.register(Profile)
+admin.site.register(Race)
+admin.site.register(WowClass)
+admin.site.register(WowSpec)
+admin.site.register(FightStyle)
+admin.site.register(SimulationType)
+admin.site.register(Simulation)
+admin.site.register(Queue)
+admin.site.register(Result)
+admin.site.register(GeneralResult)
