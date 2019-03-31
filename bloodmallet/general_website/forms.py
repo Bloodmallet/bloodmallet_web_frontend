67ecbde8d4642b279a4b7d9d5cf839497152591d
Bloodmallet <kruse.peter.1990@gmail.com>
2019-03-31 16:51:16 +0200

    add SimulationCreationForm

--- bloodmallet/general_website/forms.py
+++ bloodmallet/general_website/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 
-from general_website.models import User
+from general_website.models import User, Simulation, SimulationType, WowSpec
 
 # class UserLoginForm(forms.ModelForm):
 #     class Meta:
@@ -59,3 +59,24 @@ class ProfileUpdateForm(forms.ModelForm):
         help_texts = {
             'bloodytext': "Allow others to see your short message below the title.",
         }
+
+
+class SimulationCreationForm(forms.ModelForm):
+
+    class Meta:
+        model = Simulation
+        fields = (
+            'name',
+            'wow_spec',
+            'simulation_type',
+            'fight_style',
+        )
+
+        # widgets = {
+        #     'wow_spec': forms.CheckboxSelectMultiple(),
+        # }
+
+    def __init__(self, *args, **kwargs):
+        super(SimulationCreationForm, self).__init__(*args, **kwargs)
+        self.fields['simulation_type'].queryset = SimulationType.objects.order_by('name')     # pylint: disable=no-member
+        self.fields['wow_spec'].queryset = WowSpec.objects.order_by('wow_class__pretty_name', 'pretty_name')     # pylint: disable=no-member
