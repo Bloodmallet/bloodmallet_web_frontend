bba5cad1daf9933933eac153c1a8b5499daf9930
Bloodmallet <kruse.peter.1990@gmail.com>
2019-04-20 15:04:49 +0200

    split translation generation from deployment

--- deploy.ps1
+++ deploy.ps1
@@ -11,15 +11,6 @@ Write-Host "    Done" -ForegroundColor Green
 # Navigate to necessary subdirectory (which has the actual app for the appengine)
 cd bloodmallet/
 
-Write-Host "Preparing translations" -NoNewline
-# collect and compile translateable texts
-$languages = @('cn', 'de', 'es', 'fr', 'it', 'ko', 'pt', 'ru')
-foreach($language in $languages) {
-    python manage.py makemessages --locale=$language -i "env" >$null
-    python manage.py compilemessages --locale=$language >$null
-}
-Write-Host "          Done" -ForegroundColor Green
-
 Write-Host "Preparing Styles" -NoNewline
 # Create fresh css files
 python manage.py compilescss >$null
