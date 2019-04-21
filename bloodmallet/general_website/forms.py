5cb5bb970726f210dcbe0627c6d5c97db3811055
Bloodmallet <kruse.peter.1990@gmail.com>
2019-04-22 00:41:44 +0200

    format chart creation form

--- bloodmallet/general_website/forms.py
+++ bloodmallet/general_website/forms.py
@@ -1,27 +1,13 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.utils.translation import gettext_lazy as _
 
 from general_website.models import User, Simulation, SimulationType, WowSpec
 
-# class UserLoginForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = (
-#             'email',
-#             'password'
-#         )
-#         widgets = {
-#             'password': forms.PasswordInput(),
-#         }
-#         help_texts = {
-#         #     'email': "Enter Email here...",
-#         #     'password': "Enter Password here...",
-#         }
-
-#     def __init__(self, *args, **kwargs):
-#         super(UserLoginForm, self).__init__(*args, **kwargs)
-#         self.fields['email'].required = True
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Div
+from crispy_forms.bootstrap import FieldWithButtons, StrictButton
 
 
 class SignUpForm(UserCreationForm):
@@ -70,6 +56,7 @@ class SimulationCreationForm(forms.ModelForm):
             'wow_spec',
             'simulation_type',
             'fight_style',
+            'character_input',
         )
 
         # widgets = {
@@ -80,3 +67,19 @@ class SimulationCreationForm(forms.ModelForm):
         super(SimulationCreationForm, self).__init__(*args, **kwargs)
         self.fields['simulation_type'].queryset = SimulationType.objects.order_by('name')     # pylint: disable=no-member
         self.fields['wow_spec'].queryset = WowSpec.objects.order_by('wow_class__pretty_name', 'pretty_name')     # pylint: disable=no-member
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                Div(Field('name'), css_class='col-12 col-md-6'),
+                Div(Field('wow_spec'), css_class='col-12 col-md-6'),
+                css_class='row'
+            ),
+            Div(
+                Div(Field('simulation_type'), css_class='col-12 col-md-6'),
+                Div(Field('fight_style'), css_class='col-12 col-md-6'),
+                css_class='row'
+            ),
+            Div(Div(Field('character_input'), css_class='col-12'), css_class='row'),
+            StrictButton(_("Create Chart"), css_class="btn btn-primary")
+        )
