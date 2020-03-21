40a61158c81d00a1105c605851a43406ed4dd2f5
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-03-21 15:25:10 +0100

    remove patron information on user removal action

--- general_website/models/account.py
+++ general_website/models/account.py
@@ -6,6 +6,7 @@ from typing import Tuple
 from allauth.socialaccount.signals import pre_social_login
 from allauth.socialaccount.signals import social_account_added
 from allauth.socialaccount.signals import social_account_updated
+from allauth.socialaccount.signals import social_account_removed
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.signals import user_logged_in
@@ -16,11 +17,17 @@ from django.utils.translation import gettext_lazy as _
 
 logger = logging.getLogger(__name__)
 
-PATRON_LEVELS = [
+PATRON_TIERS = [
     'Uncommon',
     'Rare',
     'Epic',
 ]
+
+# allauth docs
+# https://django-allauth.readthedocs.io/en/latest/overview.html
+
+# patron docs
+# https://docs.patreon.com/#apiv2-resource-endpoints
 
 
 # Create your models here.
@@ -28,7 +35,10 @@ class User(AbstractUser):
     """Userdata of bloodmallet.com.
     """
     id = models.UUIDField(
-        default=uuid.uuid4, editable=False, help_text="Uuid used to identify a User.", primary_key=True
+        default=uuid.uuid4,
+        editable=False,
+        help_text="Uuid used to identify a User.",
+        primary_key=True
     )
     bloodytext = models.CharField(max_length=10, blank=True)
     patron_name = models.CharField(max_length=100, blank=True, help_text=_("Patron Name"))
@@ -45,7 +55,7 @@ class User(AbstractUser):
             return True
         if self.is_staff:
             return True
-        if self.patron_tier in PATRON_LEVELS:
+        if self.patron_tier in PATRON_TIERS:
             return True
         if self.is_guide_writer:
             return True
@@ -58,24 +68,18 @@ class User(AbstractUser):
 
 
 @receiver([pre_social_login, social_account_updated])
-def update_pledge_level(request, sociallogin, **kwargs):
+def update_patron_tier(request, sociallogin, **kwargs):
     """
-    Checks kwargs for pledge level in account data.
-    TODO: Add actual functionality to grab patreon level
-
-    Find info here:
-        https://www.patreon.com/portal/registration/register-clients
-        https://docs.patreon.com/#third-party-libraries
-        patreons own lib https://github.com/Patreon/patreon-python
-        https://django-allauth.readthedocs.io/en/latest/signals.html#allauth-socialaccount
+    Checks kwargs for patron tier in account data.
 
     Arguments:
         sender {[type]} -- Model
         sociallogin {[type]} -- instance
     """
+    logger.debug('update_patron_tier: signal received')
 
     request.user.patron_name = sociallogin.account.extra_data['attributes']['full_name']
-    request.user.patron_level = sociallogin.account.extra_data['pledge_level'] or ''
+    request.user.patron_tier = sociallogin.account.extra_data['pledge_level'] or ''
     request.user.save()
 
 
@@ -93,11 +97,27 @@ def update_user_information(sender, request, user, **kwargs):
         user {User} -- User model data blop
     """
 
+    # API = 'https://www.patreon.com/api/oauth2/v2/members/{id}?include=currently_entitled_tiers&fields%5Btier%5D=title'
     logger.debug("User logged in!")
     logger.debug(sender)
     logger.debug(request)
     logger.debug(user)
     logger.debug(user.email)
+
+
+@receiver(social_account_removed)
+def remove_patron_information(request, socialaccount, **kwargs):
+    """Remove social account information from ex-Patrons.
+
+    Args:
+        request ([type]): [description]
+        socialaccount ([type]): [description]
+    """
+    logger.debug('remove_patron_information: signal received')
+
+    request.user.patron_name = ''
+    request.user.patron_tier = ''
+    request.user.save()
 
 
 # https://docs.patreon.com/#fetching-a-patron-39-s-profile-info
