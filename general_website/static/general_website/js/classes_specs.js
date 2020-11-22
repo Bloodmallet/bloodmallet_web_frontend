b4eec619d187484f6ef3f544d1cbf30969709dde
Bloodmallet <bloodmalleteu@gmail.com>
2020-11-22 19:07:08 +0000

    provide temporary classes_specs data

---
+++ general_website/static/general_website/js/classes_specs.js
@@ -0,0 +1,38 @@
+let classes_specs = {
+    "death_knight": [
+        "blood", "frost", "unholy"
+    ],
+    "demon_hunter": [
+        "havoc", "vengeance"
+    ],
+    "druid": [
+        "feral", "guardian", "balance"
+    ],
+    "hunter": [
+        "beast_mastery", "marksmanship", "survival"
+    ],
+    "mage": [
+        "arcane", "fire", "frost"
+    ],
+    "monk": [
+        "brewmaster", "windwalker"
+    ],
+    "paladin": [
+        "protection", "retribution"
+    ],
+    "priest": [
+        "shadow"
+    ],
+    "rogue": [
+        "assassination", "outlaw", "subtlety"
+    ],
+    "shaman": [
+        "elemental", "enhancement"
+    ],
+    "warlock": [
+        "affliction", "demonology", "destruction"
+    ],
+    "warrior": [
+        "arms", "fury", "protection"
+    ]
+};
