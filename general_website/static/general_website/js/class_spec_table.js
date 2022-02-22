2e8df2789758d7f6c36bac50cfce31c9487cd886
Bloodmallet <bloodmalleteu@gmail.com>
2022-02-22 14:52:09 +0100

    enable some specs

--- general_website/static/general_website/js/class_spec_table.js
+++ general_website/static/general_website/js/class_spec_table.js
@@ -54,7 +54,7 @@ function build_table() {
                 // "demon_hunter_havoc",
                 // "demon_hunter_vengeance",
                 // "druid_balance",
-                // "druid_feral",
+                "druid_feral",
                 // "druid_guardian",
                 // "hunter_beast_mastery",
                 // "hunter_marksmanship",
@@ -75,8 +75,8 @@ function build_table() {
                 "warlock_affliction",
                 "warlock_demonology",
                 "warlock_destruction",
-                // "warrior_arms",
-                // "warrior_fury",
+                "warrior_arms",
+                "warrior_fury",
                 // "warrior_protection"
             ].indexOf(wow_class + "_" + wow_spec) > -1) {
                 a_spec_btn.className = 'spec-btn ' + wow_class + '-button col-12 translate_' + wow_spec;
