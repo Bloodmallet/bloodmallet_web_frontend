e35b10c0cb278354672828b718eec33cd3aeabd4
Peter Kruse <kruse.peter.1990@gmail.com>
2022-12-08 13:00:30 +0100

    enable blood DK

--- general_website/static/general_website/js/class_spec_table.js
+++ general_website/static/general_website/js/class_spec_table.js
@@ -48,7 +48,7 @@ function build_table() {
             let a_spec_btn = document.createElement('a');
             // It's a simulated spec
             if ([
-                // "death_knight_blood",
+                "death_knight_blood",
                 "death_knight_frost",
                 "death_knight_unholy",
                 // "demon_hunter_havoc",
